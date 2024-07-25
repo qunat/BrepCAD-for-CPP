6582bb655b632c2e21135d8bf5c7816c05a34693
unknown <304817449@qq.com>
2024-07-26 05:41:21 +0800

    debug sketcher

--- src/Sketcher/Sketchermanager.cpp
+++ src/Sketcher/Sketchermanager.cpp
@@ -1,5 +1,7 @@
 #include "Sketchermanager.h"
 #include "../DockModelTree.h"
+#include <Geom_Plane.hxx>
+#include <gp_Pln.hxx>
 
 Sketchermanager::Sketchermanager(MainWindow*par)
 {
@@ -22,8 +24,13 @@ void Sketchermanager::Showgui()
 void Sketchermanager::mySlot()
 {
 	parent->DisplayCoreManeger[parent->windownsmanager->current_windown]->Context->InitDetected();
-	auto shape = parent->DisplayCoreManeger[parent->windownsmanager->current_windown]->Context->DetectedCurrentObject();// 通过此方法可以当前鼠标点击的ais_shape
-	//auto shape = shape.get();
+	auto plane = parent->DisplayCoreManeger[parent->windownsmanager->current_windown]->Context->DetectedCurrentObject();// 通过此方法可以当前鼠标点击的ais_shape
+	Handle(AIS_Plane) anAISShape = Handle(AIS_Plane)::DownCast(plane);
+	Handle(Geom_Plane) geomplane = anAISShape->Component();
+	gp_Pln pln = geomplane->Pln();
+	gp_XYZ   asis = pln.Axis().Direction().XYZ();
+	//auto plane = shape.DownCast;																								//auto shape = shape.get();
 	//Handle(AIS_Shape) ais_shape = new AIS_Shape(shape.Reversed());
+	//AIS_Plane plane = AIS_Plane::(shape); // get the TopoDS_Shape
 
 }
